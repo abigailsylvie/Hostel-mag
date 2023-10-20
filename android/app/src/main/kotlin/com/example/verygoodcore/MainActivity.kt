c112c7e035f3a2b3c9f4e61a896dd25a81a1ce11
baimamboukar <baimamboukar@gmail.com>
2023-10-20 12:58:46 +0100

    chore: init flutter project

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.hostelmag
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:hostelmag/app/app.dart';
+import 'package:hostelmag/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
